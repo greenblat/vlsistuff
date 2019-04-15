25bbafce6aa7f4b36210f6063f60893eeb42c5f1
ilia greenblat <greenblat@mac.com>
2019-04-15 20:48:55 +0300

    all new

--- pybin/recordActivity.py
+++ pybin/recordActivity.py
@@ -8,10 +8,11 @@ def record():
     Script  = sys.argv[0] 
     Date = datetime.date.today()
     DDD = str(Date.day)+'_'+str(Date.month)+'_'+str(Date.year)
+    NewName = os.path.expanduser('~')
     try:
-        Frec = open('~/records.python','a')
+        Frec = open('%s/records.python'%NewName,'a')
     except:
-        Frec = open('~/records.python','w')
+        Frec = open('%s/records.python'%NewName,'w')
     Frec.write('%s %s %s\n'%(Script,Date,string.join(sys.argv,' ')))
     Frec.close()
 
