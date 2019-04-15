64e0b5ec90c892603a62999fb3168e9c44906461
ilia greenblat <greenblat@mac.com>
2019-04-15 20:43:12 +0300

    all

--- pybin/recordActivity.py
+++ pybin/recordActivity.py
@@ -8,7 +8,7 @@ def record():
     Script  = sys.argv[0] 
     Date = datetime.date.today()
     DDD = str(Date.day)+'_'+str(Date.month)+'_'+str(Date.year)
-    Frec = open('/Users/ilia/records.python','a')
+    Frec = open('~/records.python','a')
     Frec.write('%s %s %s\n'%(Script,Date,string.join(sys.argv,' ')))
     Frec.close()
 
