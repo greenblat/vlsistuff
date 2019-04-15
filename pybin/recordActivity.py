c8a0247d4db3dfa56df1da415d35f1547e20c592
ilia greenblat <greenblat@mac.com>
2019-04-15 20:46:05 +0300

    all

--- pybin/recordActivity.py
+++ pybin/recordActivity.py
@@ -8,7 +8,10 @@ def record():
     Script  = sys.argv[0] 
     Date = datetime.date.today()
     DDD = str(Date.day)+'_'+str(Date.month)+'_'+str(Date.year)
-    Frec = open('~/records.python','a')
+    try:
+        Frec = open('~/records.python','a')
+    except:
+        Frec = open('~/records.python','w')
     Frec.write('%s %s %s\n'%(Script,Date,string.join(sys.argv,' ')))
     Frec.close()
 
