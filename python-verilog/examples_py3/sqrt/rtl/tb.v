1c0b4946636475676ec9b7087058dea56d370c6e
ilia greenblat <greenblat@mac.com>
2020-08-13 09:11:40 +0300

    added examples_py3 and vpi3

---
+++ python-verilog/examples_py3/sqrt/rtl/tb.v
@@ -0,0 +1,32 @@
+module tb;
+reg [31:0] cycles;   initial cycles=0;
+reg [31:0] errors;   initial errors=0;
+reg [31:0] wrongs;   initial wrongs=0;
+reg [31:0] corrects; initial corrects=0;
+reg [31:0] marker;   initial marker=0;
+reg [39:0] ain;
+reg  clk,en,rst_n,vldin;
+wire [19:0] out;
+wire  vldout;
+
+always begin
+    clk=0;
+    #10;
+    clk=1;
+    #3; $python("negedge()"); #7;
+end
+initial begin
+    $dumpvars(0,tb);
+    ain[39:0] = 0;
+    en = 0;
+    rst_n = 0;
+    vldin = 0;
+    #100;
+    rst_n=1;
+end
+sqrt_u40_5 sqrt_u40_5 (
+     .clk(clk) ,.rst_n(rst_n) ,.en(en)
+    ,.ain(ain[39:0]) ,.vldin(vldin)
+    ,.out(out[19:0]) ,.vldout(vldout)
+);
+endmodule
---
+++ python-verilog/examples_py3/sqrt/verilog.py
@@ -0,0 +1,47 @@
+
+
+import os,sys,string,random,math
+import veri
+sys.path.append('../../../verification_libs3')
+import logs
+
+cycles=0
+state='idle'
+def negedge():
+    global cycles,idle,Ain,state,Max
+    cycles += 1
+    veri.force('tb.cycles',str(cycles))
+    if (cycles==1000):
+        veri.listing('tb','1000','cucu.list')
+        logs.finishing('ENOUGH CYCLES')
+        return
+    veri.force('tb.en','1')
+
+    rst_n = veri.peek('tb.rst_n')
+    if (rst_n!='1'):
+        ain = veri.peek('tb.ain')
+        Len = len(ain)
+        Max = 1<<Len
+        return
+
+
+    if state=='idle':
+        Ain = random.randint(0,Max)
+        veri.force('tb.ain',logs.make_str(Ain))
+        veri.force('tb.vldin','1')
+        state='work0'
+    elif state=='work0':
+        veri.force('tb.vldin','0')
+        state='wait0'
+    elif state=='wait0':
+        vldout = logs.peek('tb.vldout')
+        if vldout==1:
+            Out = logs.peek('tb.out')
+            Exp = int(math.sqrt(Ain))
+            if Out==Exp:
+                logs.log_correct('in=%d out=%d'%(Ain,Out))
+            else:
+                logs.log_wrong('ain=%d max=%d expected=%d act=%d'%(Ain,Max,int(math.sqrt(Ain)),Out))
+            state='idle'
+
+
