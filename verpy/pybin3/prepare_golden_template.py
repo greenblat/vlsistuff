47e404b8f0c6af144e8b616addd13e3b03e946d5
ilia greenblat <greenblat@mac.com>
2020-12-30 14:48:26 +0200

    golden model helpers

--- verpy/pybin3/prepare_golden_template.py
+++ verpy/pybin3/prepare_golden_template.py
@@ -7,7 +7,11 @@ import logs
 class MODULEClass(logs.driverClass):
     def __init__(self,Path,Monitors):
         logs.driverClass.__init__(self,Path,Monitors)
+        self.Observing = True
     def run(self):
+        self.incoming()
+        self.outgoing()
+    def incoming(self):
 '''
 
 # driverMonitor('tb',Monitors)
@@ -22,6 +26,14 @@ def help_main(Env):
         Dir,_ = Mod.nets[Net]
         if 'input' in Dir:
             Str += '        %s = self.peek("%s")\n'%(Net,Net)
+    
+    Str += '     def outgoing(self):\n        if (self.Observing):\n'
+    for Net in Mod.nets:
+        Dir,_ = Mod.nets[Net]
+        if 'output' in Dir:
+            Str += '             %s = self.peek("%s")\n'%(Net,Net)
+
+    Str += '             return\n'
     for Net in Mod.nets:
         Dir,_ = Mod.nets[Net]
         if 'output' in Dir:
