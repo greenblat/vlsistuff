01909d1a6f87de2c4b8de20f6a821c04f8b34712
Ilia <30899998+greenblat@users.noreply.github.com>
2023-10-11 22:04:42 +0300

    COMMITMENTS

--- verification_libs3/sequenceClass.py
+++ verification_libs3/sequenceClass.py
@@ -449,7 +449,7 @@ class sequenceClass:
             if len(wrds)==3:
                 Val = self.eval(wrds[2])
                 self.Translates[Var]=Val
-                logs.log_info('DBG %s %s "%s"   %s' % (Var,Val,wrds,logs.within(self.Translates)))
+                logs.log_info('DBG %s %s "%s"   %s' % (Var,Val,wrds,logs.without(self.Translates)))
             elif len(wrds)>3:
                 Val = list(map(self.eval,wrds[2:]))
                 self.Translates[Var]=Val
