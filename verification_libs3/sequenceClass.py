f30b0d099c39d6746e73c06bf93dc0a0f64ba3a5
Ilia <30899998+greenblat@users.noreply.github.com>
2026-07-08 07:57:59 +0300

    COMMITMENTS

--- verification_libs3/sequenceClass.py
+++ verification_libs3/sequenceClass.py
@@ -760,7 +760,7 @@ class sequenceClass:
             BB = makeExpr(wrds[1])
             Val = self.evalExpr(BB)
             if type(Val) is str:
-                logs.log_error("EVALUNTIL %s BB=%s Val=%s   lnum=%s" % (wrds[1],BB,Val,lnum))
+                logs.log_error("EVALUNTIL Val gets string  %s BB=%s Val=%s   lnum=%s" % (wrds[1],BB,Val,lnum))
                 return True
             if not Val: 
                 self.Ptr -= 1
